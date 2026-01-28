eca340fd5c6783f4304f929763e3ba8034b77305
bela-jere <bj005675@tamu.edu>
2026-01-27 20:53:43 -0600

    Update two_sum.cpp

--- arrays/two_sum.cpp
+++ arrays/two_sum.cpp
@@ -8,9 +8,7 @@ Space Complexity: O(1)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int current_value;
         for (int i = 0; i < nums.size(); i++) {
-            current_value = nums.at(i);
             for (int j = i + 1; j < nums.size(); j++) {
                 if (nums.at(i) + nums.at(j) == target) {
                     return {i, j};
@@ -19,3 +17,4 @@ public:
         }
     }
 };
+
